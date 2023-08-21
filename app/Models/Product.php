1c0e6ae501361f0a3cd6206c917ac8b8cdbc5a98
mowahed400 <60117806+mowahed400@users.noreply.github.com>
2023-08-21 17:29:53 +0300

    relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $guarded;
+    public  function category(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function productColorSize(){
+        return $this->hasMany(ProductColorSize::class, 'product_id');
+    }
+
+    public function productColor(){
+        return $this->hasMany(ProductColor::class, 'product_id');
+    }
+
+    public function productSize(){
+        return $this->hasMany(ProduceSize::class, 'product_id');
+    }
 }
