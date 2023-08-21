c198ef1e9907665dc574461fd7b30537057b2d27
mowahed400 <60117806+mowahed400@users.noreply.github.com>
2023-08-21 16:09:03 +0300

    migrate

---
+++ app/Http/Controllers/ProduceImageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProduceImage;
+use Illuminate\Http\Request;
+
+class ProduceImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProduceImage $produceImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProduceImage $produceImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProduceImage $produceImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProduceImage $produceImage)
+    {
+        //
+    }
+}
